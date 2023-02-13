d0878c0a07489a51b9efa8f5b70eab2c823677e4
Ajay saini <ajaysaini0505@gmail.com>
2023-02-13 20:39:01 +0530

    removed href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,16 +12,16 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <a className="nav-link" href="/">Link</a>
-                            </li>
+                            </li> */}
                         </ul>
-                        <form className="d-flex" role="search">
+                        {/* <form className="d-flex" role="search">
                             <input className={`form-control me-2 bg-${props.mode === 'light' ? 'light' : 'secondary'}`} type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-danger mx-2" type="submit">Search</button>
-                        </form>
+                        </form> */}
                     </div>
                     <div className={`form-check form-switch my-2 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
